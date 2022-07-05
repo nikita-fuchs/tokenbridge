4ccb43751846a6a04c20a9a246dfabd0cba50af7
Nikita Fuchs <q.fuchs@gmail.com>
2022-07-05 14:10:46 +0200

    adjust router & side menu, add link indicator icons

--- Frontend/src/app/ui/ui.routes.ts
+++ Frontend/src/app/ui/ui.routes.ts
@@ -7,8 +7,8 @@ import {HomeComponent} from "./home/home.component";
 import {TransactionComponent} from "./transaction/transaction.component";
 
 export const UiRoute: Routes = [
-  { path: '', redirectTo: 'money', pathMatch: 'full'},
-  { path: 'money', component: TransactionComponent },
+  { path: '', redirectTo: 'mint', pathMatch: 'full'},
+  { path: 'mint', component: TransactionComponent },
   { path: 'home', component: HomeComponent},
   { path: 'account', component: AccountComponent},
   { path: '404', component: ErrorComponent },
